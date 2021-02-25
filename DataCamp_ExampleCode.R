94ad98d6edfc9c0b9b773b114eaa169e3968559d
markwarren-uk <amarkwarren@gmail.com>
2021-02-25 09:24:07 +0000

    All DataCamp app code added

--- DataCamp_ExampleCode.R
+++ DataCamp_ExampleCode.R
@@ -1,6 +1,8 @@
 #THIS SI THE CODE FROM THE DATACAMP COURSE - USE THIS AS BASISI FOR DESIGNING UI ETC
 #WILL NEED TO DOWNLOAD AND EDIT DATA BEFORE APP WRITTEN
 
+
+#CHOOSING STATE AND DATE RANGE
 ui <- fluidPage(
     # CODE BELOW: Add a title
     titlePanel("UFO Sightings"),  
@@ -24,3 +26,114 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
+
+
+#CHOOSING SHAPE AND ADDING TABLE
+server <- function(input, output) {
+    # CODE BELOW: Create a plot output name 'shapes', of sightings by shape,
+    # For the selected inputs
+    output$shapes <- renderPlot({
+        usa_ufo_sightings %>%
+            filter(state == input$state,
+                   date_sighted >= input$dates[1],
+                   date_sighted <= input$dates[2]) %>%
+            ggplot(aes(shape)) +
+            geom_bar() +
+            labs(x = "Shape", y = "# Sighted")
+    })
+    # CODE BELOW: Create a table output named 'duration_table', by shape, 
+    # of # sighted, plus mean, median, max, and min duration of sightings
+    # for the selected inputs
+    output$duration_table <- renderTable({
+        usa_ufo_sightings %>%
+            filter(
+                state == input$state,
+                date_sighted >= input$dates[1],
+                date_sighted <= input$dates[2]
+            ) %>%
+            group_by(shape) %>%
+            summarize(
+                nb_sighted = n(),
+                avg_duration = mean(duration_sec),
+                median_duration = median(duration_sec),
+                min_duration = min(duration_sec),
+                max_duration = max(duration_sec)
+            )
+    })
+}
+
+ui <- fluidPage(
+    titlePanel("UFO Sightings"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("state", "Choose a U.S. state:", choices = unique(usa_ufo_sightings$state)),
+            dateRangeInput("dates", "Choose a date range:",
+                           start = "1920-01-01",
+                           end = "1950-01-01")
+        ),
+        mainPanel(
+            # Add plot output named 'shapes'
+            plotOutput("shapes"),
+            # Add table output named 'duration_table'
+            tableOutput("duration_table")
+        )
+    )
+)
+
+shinyApp(ui, server)
+
+#TAB LAYOUT
+ui <- fluidPage(
+    titlePanel("UFO Sightings"),
+    sidebarPanel(
+        selectInput("state", "Choose a U.S. state:", choices = unique(usa_ufo_sightings$state)),
+        dateRangeInput("dates", "Choose a date range:",
+                       start = "1920-01-01",
+                       end = "1950-01-01"
+        )
+    ),
+    # MODIFY CODE BELOW: Create a tab layout for the dashboard
+    mainPanel(tabsetPanel(
+        tabPanel('UFO sightings plot',plotOutput("shapes")),
+        tabPanel('UFO sightings table', tableOutput("duration_table"))
+    )    
+    )
+)
+
+server <- function(input, output) {
+    output$shapes <- renderPlot({
+        usa_ufo_sightings %>%
+            filter(
+                state == input$state,
+                date_sighted >= input$dates[1],
+                date_sighted <= input$dates[2]
+            ) %>%
+            ggplot(aes(shape)) +
+            geom_bar() +
+            labs(
+                x = "Shape",
+                y = "# Sighted"
+            )
+    })
+    
+    output$duration_table <- renderTable({
+        usa_ufo_sightings %>%
+            filter(
+                state == input$state,
+                date_sighted >= input$dates[1],
+                date_sighted <= input$dates[2]
+            ) %>%
+            group_by(shape) %>%
+            summarize(
+                nb_sighted = n(),
+                avg_duration_min = mean(duration_sec) / 60,
+                median_duration_min = median(duration_sec) / 60,
+                min_duration_min = min(duration_sec) / 60,
+                max_duration_min = max(duration_sec) / 60
+            )
+    })
+}
+
+shinyApp(ui, server)
+
+
